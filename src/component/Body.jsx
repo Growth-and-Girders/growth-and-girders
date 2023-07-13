eb8396cf0fdc10406fff85fc6c00480fa9e569e0
0xnehasingh.eth <67492324+0xnehasingh@users.noreply.github.com>
2023-07-13 11:39:12 +0530

    added navigation

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -1,22 +1,38 @@
-import React from 'react';
 import body from "../Assets/body.png";
 import "./Navbar.css";
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 
 const Body = () => {
+  let navigate = useNavigate() ;
   return (
-    
     <>
-    <div style={{ color: "white", backgroundcolor: 'black', paddingLeft: '120px', paddingTop: '60px', fontSize: '30px'}}>
-        Together, let's create a more sustainable <br/>
-        future through inclusive finance and <br/>
+      <div
+        style={{
+          color: "white",
+          backgroundcolor: "black",
+          paddingLeft: "120px",
+          paddingTop: "60px",
+          fontSize: "30px",
+        }}
+      >
+        Together, let's create a more sustainable <br />
+        future through inclusive finance and <br />
         Climate Action.
-        
-        
       </div>
-    <img src={body} alt="najgjh"></img>
-    </>
-  )
-}
+      <button
+        type="button"
+        className="bg-blue-500 hover:bg-blue-900 text-white rounded-md px-6 py-2 text-sm font-medium ml-40"
+        style={{ marginTop: "40px" }}
+        onClick={() => {
+          navigate("/Dashboard");
+        }}> Launchpad
+      </button>
 
-export default Body
+      <img src={body} alt="najgjh" />
+    </>
+  );
+};
+
+export default Body;
